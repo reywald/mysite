118f0f51fd2dcc3f7f66bc78dab87a21569089c6
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2023-03-06 11:44:37 +0100

    Build class-based views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.xhtml"
 
 
 def post_list(request):
